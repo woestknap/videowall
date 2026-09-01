e43b5481caa454509767a43cb3dbb84487dd8c79
woestknap <ruuddejong@gmail.com>
2026-09-01 15:29:29 +0200

    Bundle consistent scene fonts across players

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/space-grotesk/400.css'
+import '@fontsource/space-grotesk/700.css'
+import '@fontsource/source-serif-4/400.css'
+import '@fontsource/jetbrains-mono/400.css'
 import App from './App'
 import './styles.css'
 
